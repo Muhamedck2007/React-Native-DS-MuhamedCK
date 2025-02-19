af809a24cb6be2f4de03f59a96f3bfe84d100e82
Muhamedck2007 <145607038+Muhamedck2007@users.noreply.github.com>
2025-02-19 20:53:00 +0100

    challenge

--- challenge1/App.js
+++ challenge1/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+mport {NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Info Personale</Text>
+      <text>Emri:Muhamed</text>
+      <text>Mbiemri:Cakaj</text>
+      <text>Age: 17</text>
+      <text>Birth date:26/09/2007</text>
+      <text>School:Eqrem qabej</text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +23,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
